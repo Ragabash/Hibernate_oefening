6864341f435c6075921b97984e920e0dc6198e43
Ragabash <ragabash009@gmail.com>
2014-12-03 14:51:04 +0100

    Lagen toegevoegd
    Packages + lagen toegevoegd aan het project

--- Hibernate_Oefening/src/hibernate_oefening/Hibernate_Oefening.java
+++ Hibernate_Oefening/src/hibernate_oefening/Hibernate_Oefening.java
@@ -16,7 +16,9 @@ public class Hibernate_Oefening {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+      
+         
+       }
+    
     
 }
